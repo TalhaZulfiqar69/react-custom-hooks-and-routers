f71c026fe470a858aa9ad14ea481a59669d8f9a9
TalhaZulfiqar69 <talharana69@yahoo.com>
2021-02-09 12:14:12 +0500

    cors issue resolved

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,27 @@
 const functions = require('firebase-functions')
 const firebase = require('firebase-admin')
+const cors = require('cors')({ origin: true })
 
 // http://localhost:5001/training-project-be1c1/us-central1/helloWorld
 
-exports.getFirstAndLastName = functions.https.onRequest((request, response) => {
-    functions.logger.info('getFirstAndLastName logs!', { structuredData: true })
+exports.getFirstAndLastName = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+        functions.logger.info('getFirstAndLastName logs!', { structuredData: true })
 
-    const { firstName, lastName } = request.body
-    console.log('firstName', firstName)
-    console.log('lastName', lastName)
-    console.log('Full Name', firstName + ' ' + lastName)
-    response.send('Firebase helloWorld Function')
+        console.log('req.body', req.body)
+        const { fName, lName } = req.body
+        console.log('fName', fName)
+        console.log('lName', lName)
+        console.log('Full Name', fName + ' ' + lName)
+        res.send('Firebase helloWorld Function')
+        // res.send('Passed.')
+    })
 })
 
 // exports.byeWorld = functions.https.onRequest((request, response) => {
 //     functions.logger.info("Bye logs!", {structuredData: true});
 //     response.send("Bye from Firebase!");
 //   });
+
+// difference between express router and
+// cloudfunctuons routers
